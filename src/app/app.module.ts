be6902122a3f8935a9d7c38e6b46729051fb49b8
llckumadevelop <kumauser01@kuma.ink>
2024-03-29 10:38:11 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent } from './hero-parent.component';
+import { NameChildComponent } from './name-child.component';
+import { NameParentComponent } from './name-parent.component';
+import { VersionChildComponent } from './version-child.component';
+import { VersionParentComponent } from './version-parent.component';
+import { VoterComponent } from './voter.component';
+import { VoteTakerComponent } from './votetaker.component';
+import { CountdownTimerComponent } from './countdown-timer.component';
+import { CountdownLocalVarParentComponent } from './countdown-parent.component';
+import { CountdownViewChildParentComponent } from './countdown-parent.vc.component';
+import { MissionControlComponent } from './missioncontrol.component';
+import { AstronautComponent } from './astronaut.component';
+import { MissionService } from './mission.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent,
+    HeroParentComponent,
+    NameChildComponent,
+    NameParentComponent,
+    VersionChildComponent,
+    VersionParentComponent,
+    VoterComponent,
+    VoteTakerComponent,
+    CountdownTimerComponent,
+    CountdownLocalVarParentComponent,
+    CountdownViewChildParentComponent,
+    MissionControlComponent,
+    AstronautComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+  name: string;
+}
+export const HEROES = [
+  { name:"Dr.IQ"},
+  { name:"Magnet"},
+  { name:"Bombasto"}
+]
